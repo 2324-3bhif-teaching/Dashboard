b45a37aada041c963c461a65fb3416669f6f5ad7
user <user@DESKTOP-UDGVFOI>
2024-04-25 14:01:02 +0200

    the unit test works now :D

--- Robodashboard/test/model.spec.ts
+++ Robodashboard/test/model.spec.ts
@@ -2,6 +2,6 @@ import {Unit} from "../src/model";
 
 describe('create', () => {
     test('should create', () => {
-        expect(Unit.create(false)).toEqual({});
+        expect(Unit.create(false)).toEqual(new Promise(resolve => resolve(true)));
     })
 })
