af8c9802560a7e5ac379539f432da13aa7ab3dd5
user <user@DESKTOP-UDGVFOI>
2024-05-15 08:26:39 +0200

    remove unwanted file

--- Robodashboard/UpdateLeocloud.cmd
+++
@@ -1,5 +0,0 @@
-kubectl delete -n student-s-alesi deployment robodashboard
-kubectl delete -n student-s-alesi service robodashboard-svc
-kubectl delete -n student-s-alesi ingress robodashboard-ingress
-kubectl delete -n student-s-alesi pod -l app=robodashboard
-kubectl create -f leocloud-deploy-V2.yaml
